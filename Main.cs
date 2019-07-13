96f342c3db63a06128224c3241ff4644b9cc8e84
eldarhasandwich <hasanovicel@gmail.com>
2019-07-13 16:47:58 +1000

    commit

--- Main.cs
+++ Main.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections;
 
 namespace EldyState {
   class Program {
 
     static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
+      Dictionary<int> bDict = new Dictionary<int>();
+
+      bDict.Set("my face", 60);
+      bDict.Set("my other face", 60);
+
+      Console.WriteLine(bDict.Get("not my face"));
+      Console.WriteLine(bDict.Get("my other face"));
+      Console.WriteLine(bDict.Get("my face"));
     }
   }
 }
---
+++ tests/Dictionary.test.cs
@@ -0,0 +1,10 @@
+using System;
+
+// namespace EldyState {
+//   class Tests {
+
+//     static void Main(string[] args) {
+//       Console.WriteLine("Hello World!");
+//     }
+//   }
+// }
